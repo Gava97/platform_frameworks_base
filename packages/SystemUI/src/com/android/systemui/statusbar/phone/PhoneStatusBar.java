d1275ec5b92983a8f3b21bc955add14d2a007147
Anu6is <curtis.mayers@gmail.com>
2013-11-22 05:55:06 -0600

    [1/2] Frameworks: Optional statusbar battery icons
        * Circle battery and circle battery percent (from CM <Sven Dawitz>)
        * Battery bar percent from Google
        * Quicksetting battery tile reflects status icon (from SlimROM <kufikugel>)
    
    Part 2: packages_apps_Settings
    
    Patch set 2: clean white spaces
    Patch set 3: fix conflicts
    Patch set 4: rebased
    
    Signed-off-by: Anu6is <curtis.mayers@gmail.com>
    
    Change-Id: Ic4056d08874672263444152813a4a8e805a4abf3
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/Keyguard/res/values/tk_dimens.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -93,6 +93,7 @@ import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.BatteryMeterView;
+import com.android.systemui.BatteryCircleMeterView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
@@ -203,6 +204,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     IconMerger mNotificationIcons;
     // [+>
     View mMoreIcon;
+
+    private BatteryMeterView mBattery;
+    private BatteryCircleMeterView mCircleBattery;
 
     // expanded notifications
     NotificationPanelView mNotificationPanel; // the sliding/resizing panel within the notification window
@@ -364,6 +368,32 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     };
 
+    private final class BatteryIconSettingsObserver extends ContentObserver {
+        BatteryIconSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_BATTERY_STYLE), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateBatteryIcons();
+        }
+    }
+
+    private void updateBatteryIcons() {
+        if (mQS != null) {
+            mQS.updateBattery();
+        }
+        if (mBattery != null && mCircleBattery != null) {
+            mBattery.updateSettings();
+            mCircleBattery.updateSettings();
+        }
+    }
+
     private int mInteractingWindows;
     private boolean mAutohideSuspended;
     private int mStatusBarMode;
@@ -399,6 +429,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
         addNavigationBar();
 
+        SettingsObserver observer = new SettingsObserver(mHandler);
+        observer.observe();
+        BatteryIconSettingsObserver batteryIconObserver = new BatteryIconSettingsObserver(mHandler);
+        batteryIconObserver.observe();
+
         // Lastly, call to the icon policy to install/update all the icons.
         mIconPolicy = new PhoneStatusBarPolicy(mContext);
 
@@ -408,6 +443,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                     Settings.Global.getUriFor(SETTING_HEADS_UP), true,
                     mHeadsUpObserver);
         }
+        SettingsObserver sb = new SettingsObserver(new Handler());
+        sb.observe();
     }
 
     // ================================================================================
@@ -728,6 +765,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
         mBattery = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
         updateBatteryIcons();
+
+        mBattery = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
+        mCircleBattery = (BatteryCircleMeterView) mStatusBarView.findViewById(R.id.circle_battery);
 
         return mStatusBarView;
     }
