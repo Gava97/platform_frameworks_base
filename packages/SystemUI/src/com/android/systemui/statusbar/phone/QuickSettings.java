67a1a3d3c370c0ee0dd98b1c7748fe45054965d6
Andre Saddler <dresadd09691@gmail.com>
2014-01-09 00:21:36 -0500

    Quicksettings: USB Tether tile
    courtesy of OmniRom
    based on: http://goo.gl/0RfbFy
    
    PS3: add "private ConnectivityManager mCM;" variable
    PS4: compiles, not yet tested
    PS5: derp forgot to initialize mCM which caused a NPE
    (http://developer.android.com/reference/android/net/ConnectivityManager.html)
    PS6: fixed some connectivity manager redundancies in qs.java
    PS8: make tile temporary, only appears when connected to usb.
    PS10: fix conflicts in pa_strings and QuickSettingsModel after recents merges.
          clean up commit message's conflicts.
    PS11: Remove unnecessary resources
    PS12: Don't show tether tile is feature is not supported
    
    Signed-off-by: Andre Saddler <dresadd09691@gmail.com>
    Change-Id: I856f1a1f954544cb9e1b78f504fde8916b94235e
    
    Conflicts:
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
- * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
+ * This code has been modified. Portions copyright (C) 2013-2014 ParanoidAndroid Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -809,7 +809,87 @@ class QuickSettings {
         });
         parent.addView(alarmTile);
 
-       
+        // Usb Mode
+        final QuickSettingsBasicTile usbModeTile
+                = new QuickSettingsBasicTile(mContext);
+        usbModeTile.setTemporary(true);
+        usbModeTile.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mConnectivityManager.getTetherableWifiRegexs().length != 0) {
+                    Intent intent = new Intent();
+                    intent.setComponent(new ComponentName(
+                            "com.android.settings",
+                            "com.android.settings.Settings$TetherSettingsActivity"));
+                    startSettingsActivity(intent);
+                }
+                return true;
+            }
+        });
+
+        mModel.addUsbModeTile(usbModeTile, new QuickSettingsModel.RefreshCallback() {
+            @Override
+            public void refreshView(QuickSettingsTileView unused, State usbState) {
+                usbModeTile.setImageResource(usbState.iconId);
+                usbModeTile.setText(usbState.label);
+                usbModeTile.setVisibility(usbState.enabled ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        parent.addView(usbModeTile);
+
+        // Remote Display
+//        QuickSettingsBasicTile remoteDisplayTile
+//                = new QuickSettingsBasicTile(mContext);
+//        remoteDisplayTile.setTemporary(true);
+//        remoteDisplayTile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                collapsePanels();
+//
+//                final Dialog[] dialog = new Dialog[1];
+//                dialog[0] = MediaRouteDialogPresenter.createDialog(mContext,
+//                        MediaRouter.ROUTE_TYPE_REMOTE_DISPLAY,
+//                        new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog[0].dismiss();
+//                        startSettingsActivity(
+//                                android.provider.Settings.ACTION_WIFI_DISPLAY_SETTINGS);
+//                    }
+//                });
+//                dialog[0].getWindow().setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+//                dialog[0].show();
+//            }
+//        });
+//        mModel.addRemoteDisplayTile(remoteDisplayTile,
+//                new QuickSettingsModel.BasicRefreshCallback(remoteDisplayTile)
+//                        .setShowWhenEnabled(true));
+//        parent.addView(remoteDisplayTile);
+//
+//        if (SHOW_IME_TILE || DEBUG_GONE_TILES) {
+            // IME
+//            final QuickSettingsBasicTile imeTile
+//                    = new QuickSettingsBasicTile(mContext);
+//            imeTile.setTemporary(true);
+//            imeTile.setImageResource(R.drawable.ic_qs_ime);
+//            imeTile.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    try {
+//                        collapsePanels();
+//                        Intent intent = new Intent(Settings.ACTION_SHOW_INPUT_METHOD_PICKER);
+//                        PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, 0);
+//                        pendingIntent.send();
+//                    } catch (Exception e) {}
+//                }
+//            });
+//            mModel.addImeTile(imeTile,
+//                    new QuickSettingsModel.BasicRefreshCallback(imeTile)
+//                            .setShowWhenEnabled(true));
+//            parent.addView(imeTile);
+//        }
+
         // Bug reports
         final QuickSettingsBasicTile bugreportTile
                 = new QuickSettingsBasicTile(mContext);
