4ff35b4572650a7b376be88c89f524983f7959f5
Francesco Colella <mnemonyc@hotmail.com>
2014-05-08 15:50:12 +0200

    frameworks: Fix chameleon engine textview and...
    ... black icon issue

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2894,6 +2894,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 				mCurrentColor = mWhiteColor;
 				refresh();
 			}
+            mMustChange = false;
         }
     }
 
@@ -2916,10 +2917,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                     updateBackgroundDelayed();
                     return;
                 }
-                mMustChange = false;
             } else {
 				mStatusBarColor = mSysColor;
 			}
+            mBattery.updateBattery();
             transform(isGray(mSysColor));
             if (mTransparent) {
 				mStatusBarView.setBackgroundColor(Color.TRANSPARENT);
@@ -3043,8 +3044,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 			}
 		}
         mBattery.mChameleonBatteryColor = mCurrentColor;
-        mBattery.updateBattery();
-        mBattery.invalidate();
+        mBattery.updateSettings();
     }
 
 	private void updateBackgroundDelayed() {
