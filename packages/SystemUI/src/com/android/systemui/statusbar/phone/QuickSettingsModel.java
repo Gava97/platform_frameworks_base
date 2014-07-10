78c7aec3d08b02c65b91b669c8988ab53a931723
Francesco Colella <mnemonyc@hotmail.com>
2014-07-10 21:23:17 +0200

    Revert "3rd fix: HeadsUp Tile TEST 3"
    This reverts commit 3151c6c7fc04af8c4d7d3203ee5c1f9cd5c1eff5.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -653,15 +653,15 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
             }
         });
         mHeadsUpCallback = cb;
-        int headsupMode = Settings.System.getInt(mContext.getContentResolver(),
+        int headsup = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HEADS_UP_NOTIFICATION, 0);
-        onHeadsUpChanged(headsupMode != 0);
+        onHeadsUpChanged(headsup != 0);
     }
 
     private void setHeadsUpState(boolean enabled) {
         Settings.System.getIntForUser(
                 mContext.getContentResolver(),
-                Settings.System.HEADS_UP_NOTIFICATION, enabled ? 1 : 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.HEADS_UP_NOTIFICATION, enabled ? 1 : UserHandle.USER_CURRENT, 0);
     }
 
     public void onHeadsUpChanged(boolean enabled) {
